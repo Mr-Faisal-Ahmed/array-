3e15f4278f2f7497b61453abd83eb468fd3b0b9b
Faisal-Ahmed-Khan <51905252+Mr-Faisal-Ahmed@users.noreply.github.com>
2023-11-08 13:55:45 +0500

    array

---
+++ array.js
@@ -0,0 +1,22 @@
+//let car = ["BMW","corolla","civic","Audi"];
+//console.log(car[car.length -1]);
+//car.sort();
+//console.log(car);
+//const n= 5;
+//for(let i=0 ;i<=n; i++){
+//console.log(i);
+//}
+//let sum =0;
+//const n= 100
+//for( let i = 1; i<=n; i++){
+    //sum +=i;
+//}
+//console.log('sum',sum)
+//let arr=[1,2,3,4,5,2,6,8]
+
+//for(let i = 0; i < arr.length -1;i++){
+    //console.log(arr[i])
+//}
+//for(let i of arr){
+    //console.log(i);
+//}
